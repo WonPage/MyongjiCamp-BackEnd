880023563ca703d8a162df3260a8a333b115c3f0
jimmy0524 <10jmin04@naver.com>
2024-02-19 16:41:25 +0900

    초기설정

---
+++ src/main/java/com/won/myongjiCamp/MyongjiCampApplication.java
@@ -0,0 +1,13 @@
+package com.won.myongjiCamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyongjiCampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyongjiCampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/won/myongjiCamp/MyongjiCampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.won.myongjiCamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyongjiCampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
