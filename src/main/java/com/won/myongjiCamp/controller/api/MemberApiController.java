d874a15e4322c3b18d2a76294cbbb93fcf80517b
jimmy0524 <10jmin04@naver.com>
2024-03-03 06:10:02 +0900

    240303 jm logout,refresh

--- src/main/java/com/won/myongjiCamp/controller/api/MemberApiController.java
+++ src/main/java/com/won/myongjiCamp/controller/api/MemberApiController.java
@@ -1,13 +1,16 @@
 package com.won.myongjiCamp.controller.api;
 
+import com.won.myongjiCamp.config.jwt.JwtTokenUtil;
 import com.won.myongjiCamp.dto.request.CreateMemberDto;
 import com.won.myongjiCamp.dto.request.EmailDto;
 import com.won.myongjiCamp.dto.ResponseDto;
 import com.won.myongjiCamp.service.MemberService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,6 +26,9 @@ public class MemberApiController {
 
     private final MemberService memberService;
     private final StringRedisTemplate redisTemplate;
+
+    private final JwtTokenUtil jwtTokenUtil;
+    private final UserDetailsService userDetailsService;
 
     @PostMapping("/api/members")
     public ResponseDto saveMember(@RequestBody @Valid CreateMemberDto request) {
@@ -52,5 +59,49 @@ public class MemberApiController {
         memberService.verificationEmail(code, savedCode);
         return new ResponseDto(HttpStatus.OK.value(), "이메일 인증 성공");
     }
+
+    //refresh 토큰 인증 & access 재발급 (재발급할때 refresh도 갱신)
+    @PostMapping("/api/auth/refresh")
+    public ResponseDto refreshAndGetAuthenticationToken(HttpServletRequest request) throws Exception {
+        String authToken = request.getHeader("Authorization");
+        final String token = authToken.substring("Bearer ".length());
+        String username = jwtTokenUtil.extractUsername(token); //이메일
+        String storedRefreshToken = redisTemplate.opsForValue().get(username); //key가 email인 refresh Token 가져옴
+
+        //redis에 저장한 토큰이랑 받은 토큰 비교
+        if (storedRefreshToken != null && storedRefreshToken.equals(token)) {
+            // 새로운 Access Token 생성
+            String refreshedToken = jwtTokenUtil.generateToken(userDetailsService.loadUserByUsername(username));
+
+            // 새로운 Refresh Token 생성 및 Redis에 저장
+            String newRefreshToken = jwtTokenUtil.generateRefreshToken(userDetailsService.loadUserByUsername(username));
+            redisTemplate.opsForValue().set(username, newRefreshToken);
+            redisTemplate.expire(username, jwtTokenUtil.getRefreshExpirationTime(), TimeUnit.MILLISECONDS);
+
+            Map<String, Object> data = new HashMap<>();
+            data.put("message", "재발급 성공");
+            data.put("token", refreshedToken);
+            data.put("refreshToken", newRefreshToken);
+
+            return new ResponseDto<>(HttpStatus.OK.value(), data);
+        } else {
+            return new ResponseDto(HttpStatus.UNAUTHORIZED.value(), "재발급 실패");
+        }
+    }
+
+    //로그아웃
+    @PostMapping("/api/auth/logout")
+    public ResponseDto logout(HttpServletRequest request) {
+        String authToken = request.getHeader("Authorization");
+        final String token = authToken.substring("Bearer ".length());
+        String username = jwtTokenUtil.extractUsername(token); //이메일
+
+        // Redis에서 username에 해당하는 Refresh Token 삭제
+        redisTemplate.delete(username);
+
+        return new ResponseDto(HttpStatus.UNAUTHORIZED.value(), "로그아웃 성공");
+
+    }
+
 
 }
