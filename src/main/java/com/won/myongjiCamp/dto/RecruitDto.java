3156e2b557e1e11602be14dc83db94278d1a5d46
Jang-SoHyeon <jangso6593@gmail.com>
2024-03-07 21:44:20 +0900

    240307so

--- src/main/java/com/won/myongjiCamp/dto/RecruitDto.java
+++ src/main/java/com/won/myongjiCamp/dto/RecruitDto.java
@@ -1,6 +1,7 @@
 package com.won.myongjiCamp.dto;
 
 
+import com.won.myongjiCamp.model.board.RecruitStatus;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
@@ -21,8 +22,8 @@ public class RecruitDto {
 
 //    private Integer scrapCount;
 
-/*    @NotEmpty
-    private RecruitStatus status; //모집 중 or 모집 완료*/
+//    @NotEmpty
+    private RecruitStatus status; //모집 중 or 모집 완료
 
     @NotEmpty
     private String preferredLocation; //활동 지역
