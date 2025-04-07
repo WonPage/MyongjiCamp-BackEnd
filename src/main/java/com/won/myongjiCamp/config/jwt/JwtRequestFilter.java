26dbc611923a3f1c98ff9198a96080df6beaf0c7
jimmy0524 <10jmin04@naver.com>
2025-04-07 19:20:56 +0900

    [REFACTOR] add jwt valid check

--- src/main/java/com/won/myongjiCamp/config/jwt/JwtRequestFilter.java
+++ src/main/java/com/won/myongjiCamp/config/jwt/JwtRequestFilter.java
@@ -1,12 +1,15 @@
 package com.won.myongjiCamp.config.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.won.myongjiCamp.config.security.auth.PrincipalDetail;
 import com.won.myongjiCamp.config.security.auth.PrincipalDetailService;
+import com.won.myongjiCamp.dto.response.ResponseDto;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -29,26 +32,44 @@ public class JwtRequestFilter extends OncePerRequestFilter {
             throws ServletException, IOException {
         final String requestTokenHeader = request.getHeader("Authorization");
 
-        String username = null;
-        String jwtToken = null;
-
-        if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ")) {
-            jwtToken = requestTokenHeader.substring(7);
-            username = jwtTokenUtil.extractUsername(jwtToken);
+        if (requestTokenHeader == null) {
+            chain.doFilter(request, response);
+            return;
         }
 
-        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            PrincipalDetail userDetails = (PrincipalDetail) userDetailsService.loadUserByUsername(username);
-//            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
-
-            if (jwtTokenUtil.validateToken(jwtToken, userDetails)) {
-                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
-                        userDetails, null, userDetails.getAuthorities());
-                usernamePasswordAuthenticationToken
-                        .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-            }
+        if (!requestTokenHeader.startsWith("Bearer ")) {
+            sendErrorResponse(response, HttpStatus.UNAUTHORIZED, "잘못된 토큰 형식");
+            return;
         }
+
+        String jwtToken = requestTokenHeader.substring(7);
+
+        if (jwtTokenUtil.isTokenExpired(jwtToken)) {
+            sendErrorResponse(response, HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰");
+            return;
+        }
+
+        String username = jwtTokenUtil.extractUsername(jwtToken);
+        PrincipalDetail userDetails = (PrincipalDetail) userDetailsService.loadUserByUsername(username);
+
+        if (jwtTokenUtil.validateToken(jwtToken, userDetails)) {
+            UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                    userDetails, null, userDetails.getAuthorities());
+            usernamePasswordAuthenticationToken
+                    .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+        }
+
         chain.doFilter(request, response);
+    }
+
+    private void sendErrorResponse(HttpServletResponse response, HttpStatus status, String message)
+            throws IOException {
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        ResponseDto responseDto = new ResponseDto(status.value(), message);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(response.getWriter(), responseDto);
     }
 }
